88ef32c39e9dc0667ceae690bbf47b9343caa7c1
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-07 02:36:51 +0400

    feat: basic layout for register screen

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,12 +1,21 @@
-import { View, Text } from "react-native";
+import { View, Text, KeyboardAvoidingView, StyleSheet } from "react-native";
 import React from "react";
+import { StatusBar } from "expo-status-bar";
 
 const RegisterScreen = ({ navigation }) => {
   return (
-    <View>
-      <Text>RegisterScreen</Text>
-    </View>
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <StatusBar style="light" />
+      <Text h3 style={{ marginBottom: 50 }}>
+        Create Buzzer Account
+      </Text>
+      <View style={styles.inputContainer}></View>
+    </KeyboardAvoidingView>
   );
 };
 
 export default RegisterScreen;
+const styles = StyleSheet.create({
+  container: {},
+  inputContainer: {},
+});
