6ffc2420be498d1cfa1ed57a6a5432d08b961aa5
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-06 19:22:09 +0400

    feat: login screen basics elements

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,32 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
-
+import { Button, Input, Image } from "react-native-elements";
+import { StatusBar } from "expo-status-bar";
 const LoginScreen = () => {
   return (
     <View>
-      <Text>LoginScreen</Text>
+      <StatusBar style="light" />
+      <Image
+        source={{
+          uri: "https://cdn-icons-png.flaticon.com/512/295/295128.png",
+        }}
+        style={{ width: 200, height: 200 }}
+      />
+      <View style={StyleSheet.inputContainer}>
+        <Input placeholder="Email" autoFocus type="Email" />
+        <Input
+          placeholder="Password"
+          secureTextEntry
+          autoFocus
+          type="Password"
+        />
+      </View>
     </View>
   );
 };
 
 export default LoginScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {},
+});
