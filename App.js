1ad1128158326e6d1bb33df7c5fc38842c162ed5
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-06 18:59:48 +0400

    feat: login screen passed to navigation

--- App.js
+++ App.js
@@ -2,9 +2,15 @@ import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./screens/LoginScreen";
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
       <View style={styles.container}>
         <Text>APP WORK TEST</Text>
         <StatusBar style="auto" />
@@ -16,7 +22,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "red",
     alignItems: "center",
     justifyContent: "center",
   },
