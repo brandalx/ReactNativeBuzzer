98bd4b6c2bf7d9197c4d3232703a1e9489bc4ee6
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-07 02:45:36 +0400

    feat: placeholders for info

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,21 +1,64 @@
-import { View, Text, KeyboardAvoidingView, StyleSheet } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
+import { Input } from "react-native-elements";
 
 const RegisterScreen = ({ navigation }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+
   return (
-    <KeyboardAvoidingView behavior="padding" style={styles.container}>
-      <StatusBar style="light" />
-      <Text h3 style={{ marginBottom: 50 }}>
-        Create Buzzer Account
-      </Text>
-      <View style={styles.inputContainer}></View>
-    </KeyboardAvoidingView>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <KeyboardAvoidingView behavior="padding" style={styles.container}>
+        <StatusBar style="light" />
+        <Text h3 style={{ marginBottom: 50 }}>
+          Create Buzzer Account
+        </Text>
+        <View style={styles.inputContainer}>
+          <Input
+            placeholder="Full Name"
+            autoFocus
+            type="text"
+            value={name}
+            onChangeText={(text) => setName(text)}
+          />
+          <Input
+            placeholder="Email"
+            type="email"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+          />
+          <Input
+            placeholder="Password"
+            type="password"
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+          />
+          <Input
+            placeholder="Profile picture URL (optional)"
+            type="text"
+            value={imageUrl}
+            onChangeText={(text) => setImageUrl(text)}
+          />
+        </View>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   );
 };
 
 export default RegisterScreen;
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+  },
   inputContainer: {},
 });
