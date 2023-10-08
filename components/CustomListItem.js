eadeddb57aa533d8285cb70ec0d7ba00ca09a65a
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-08 18:00:55 +0400

    feat: list item avatar

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -1,14 +1,21 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-
+import { ListItem, Avatar } from "react-native-elements";
 const CustomListItem = () => {
   return (
-    <View>
-      <Text>CustomListItem</Text>
-    </View>
+    <ListItem>
+      <Avatar
+        rounded
+        source={{
+          uri: "https://cdn-icons-png.flaticon.com/512/1144/1144760.png",
+        }}
+      />
+    </ListItem>
   );
 };
 
 export default CustomListItem;
 
 const styles = StyleSheet.create({});
+
+// chatMessages?.[0].photoURL ||
