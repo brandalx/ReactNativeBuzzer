d2e3fe5f42cf5ec19dd62e4de33d97c7ceffef5b
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-09 03:38:58 +0400

    feat: ListItem.Content

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -3,14 +3,19 @@ import React from "react";
 import { ListItem, Avatar } from "react-native-elements";
 const CustomListItem = () => {
   return (
-    <ListItem containerStyle={{ backgroundColor: "white", width: 1000 }}>
+    <ListItem containerStyle={{ backgroundColor: "white", width: "100%" }}>
       <Avatar
         rounded
         source={{
           uri: "https://www.pngmart.com/files/23/Profile-PNG-HD.png",
         }}
       />
-      <Text>User name</Text>
+      <ListItem.Content>
+        <ListItem.Title style={{ fontWeight: "800" }}>User name</ListItem.Title>
+        <ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail">
+          This is Test Subtitle Test SubtitleTest for length
+        </ListItem.Subtitle>
+      </ListItem.Content>
     </ListItem>
   );
 };
