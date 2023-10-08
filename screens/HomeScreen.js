89620ac91a2514b5077ecbdd26974e6ce7f9785c
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-08 17:41:09 +0400

    feat: functions on logout / force return

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,37 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
+import { Button } from "react-native-elements";
+import { auth } from "../firebase";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
+  const handleSignIut = () => {
+    signOut(auth)
+      .then(() => {
+        navigation.navigate("Register");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        navigation.replace("Login");
+        console.log(user);
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <Text>
+        <Button title="Logout" onPress={handleSignIut} />
+      </Text>
     </View>
   );
 };
